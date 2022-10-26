abf89b4884344d6f6075c6aa7969396e9b6fafd1
Eduardo Ronchi <financaronchi@gmail.com>
2022-10-25 22:55:50 -0300

    Configurando multiplos ambientes e profiles

--- src/main/java/io/github/eduronchi/VendasApplication.java
+++ src/main/java/io/github/eduronchi/VendasApplication.java
@@ -2,23 +2,17 @@ package io.github.eduronchi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(
-        basePackages = {
-        "io.github.eduronchi.repository",
-        "io.github.eduronchi.service"
-})
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
 
